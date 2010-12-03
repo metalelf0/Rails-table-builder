78932955c894baa603a8c3d5e0e8ce4440529a20
Metalelf0 <metalelf0@gmail.com>
2010-12-03 08:22:46 +0100

    Added table spec

---
+++ spec/lib/table_spec.rb
@@ -0,0 +1,18 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe Table do
+  
+  before(:each) do
+      @valid_headers = ["first field", "second field"]
+      @valid_row = ["value for first field", "value for second field" ]
+  end
+  
+  it "should create a table and add data to it" do
+    Proc.new {
+      table = Table.new
+      table.add_headers @valid_headers
+      table.add_row @valid_row
+    }.should_not raise_error
+  end
+
+end
