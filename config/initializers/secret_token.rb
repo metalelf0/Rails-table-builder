9f30cbb2d46155e500c0be92ffcc7525c2fcb57d
Andrea Schiavini <metalelf0@gmail.com>
2011-01-21 14:10:16 +0100

    WIP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTableBuilder::Application.config.secret_token = '9757c610879e6f2d20623e5632a711f570cc8532441921bcf21949feaedd6a0011b31cd35dce3bd8fa0feefb51427988e0d7fd5c4d994f35fccb2dda229ee81d'
